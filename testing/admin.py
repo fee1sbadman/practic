eb0c29b0bf38948a857782f7eac9b0e76c5d409e
badman <admin@admin.com>
2019-05-30 05:11:52 +0500

    admin

--- testing/admin.py
+++ testing/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 from .models import Test,Question,Choice
 # Register your models here.
 
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'info')
 
-admin.site.register(Test)
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Test, TestAdmin)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3 
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name']}),
+        ('Date information', {'fields': ['right_choice']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
